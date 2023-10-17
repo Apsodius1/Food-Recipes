b8132e902f69f55eb436a6528bfd14a6aa683fd6
Augustin Cibotari <augustus.apso@gmail.com>
2023-10-17 14:51:54 +0300

    add button page with cool effect and random programmer page

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/text", async (req, res) => {
+  const message = await axios.get(
+    "https://programming-quotesapi.vercel.app/api/random"
+  );
+  res.render("index.ejs", { text: message.data });
 });
 
 app.listen(3000, () => console.log(`App listening on port ${port}!`));
